916f4ed735030013c4d10b8320c59fa16f834302
af3ld <alexfeldman@lclark.edu>
2016-04-12 11:55:47 -0700

    Rough draft of pokemon

--- app/src/main/java/com/lclark/pokedex/MainActivity.java
+++ app/src/main/java/com/lclark/pokedex/MainActivity.java
@@ -2,16 +2,18 @@ package com.lclark.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.activity_main_listview) ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
